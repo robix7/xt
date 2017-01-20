f11b8091a05bbb33cd8efb2b3fe977e72edd21e1
roboxt <robo.xt7aj@gmail.com>
2017-01-21 01:13:54 +0330

    Update s2.lua

--- plugins/s2.lua
+++ plugins/s2.lua
@@ -1,9 +1,13 @@
 local function run(msg, matches)
      if matches[1] == 'ای' then
           if is_sudo(msg) then   
+  send_document(org_chat_id,"./icons/7.webp", ok_cb, false)
+	elseif is_sudo(result) then
+		userrank = "Sudo ⭐⭐⭐⭐⭐"             
+               
        end
      end
-     return "بله"
+    
   end
 return {         
     patterns = {
