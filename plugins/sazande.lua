ec1a116c9c07e923270b01658eff439d2d443804
roboxt <robo.xt7aj@gmail.com>
2017-01-22 01:42:42 +0330

    Update sazande.lua

--- plugins/sazande.lua
+++ plugins/sazande.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
    if is_sudo(msg) then
       end
-    return [[ شاهین رضایی تبریزی ]]
+    return [[ سازنده رباتهای اکس تی اگه باهاشون کار دارین به پی وی @Shahin_xtbot مراجعه کنید سلام منم برسونین ]]
 end return 
 {
     patterns = {
