2a0f9e8f546b14df3c5e4ec584be08a9a729ec14
roboxt <robo.xt7aj@gmail.com>
2017-01-22 02:34:34 +0330

    Update s1.lua

--- plugins/s1.lua
+++ plugins/s1.lua
@@ -1,18 +1,18 @@
 local function run(msg, matches)
-     if matches[1] == 'بات' then
-                 
-               
-       end
-     end
-     
-   if is_sudo(msg) then
-     end
-     return " جانم "
+  if matches[1] == "saz" then
+    local file = matches[2]
+    if is_sudo(msg) then --sudo only !
+      local receiver = get_receiver(msg)
+      send_document(receiver, "./plugins/"sudo.webp", ok_cb, false)
+      else 
+        return nil
+    end
+  end
 end
-return {         
-    patterns = {
-    "^(بات)$"
+
+return {
+  patterns = {
+  "^[!/](saz) (.*)$"
   },
   run = run
 }
-
