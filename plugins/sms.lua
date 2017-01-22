e837bd4bb40aa190b4337073e0fb76a503bd4e2b
roboxt <robo.xt7aj@gmail.com>
2017-01-22 09:17:23 +0330

    Update sms.lua

--- plugins/sms.lua
+++ plugins/sms.lua
@@ -1,6 +1,6 @@
 local function run(msg, matches)
   if not is_sudo(msg) then -- For Sudoers only !
-    return 'It is For Sudo Users'
+    return 'از سیستم پیام رسانی فقط سازنده میتونه استفاده کنه'
   end
 	if matches[1] == 'sms' and is_sudo(msg) then
 		local response = matches[3]
