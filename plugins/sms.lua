d0c462ba58d2da57711c7ec8cfc6f3e8513e5e0a
roboxt <robo.xt7aj@gmail.com>
2017-01-22 09:49:29 +0330

    Update sms.lua

--- plugins/sms.lua
+++ plugins/sms.lua
@@ -3,10 +3,7 @@ local function run(msg, matches)
     return 'از سیستم پیام رسانی فقط سازنده میتونه استفاده کنه'
   end
 	if matches[1] == 'sms' and is_sudo(msg) then
-		  send_msg(extra.chatid, '"'..user#id..'" ارسال شد', ok_cb, false)
-  else
-    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
-  end
+		  return 'ارسال شد'
 end
 		local response = matches[3]
 		send_large_msg("user#id"..matches[2], response)
