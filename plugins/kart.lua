c5db209bb546ad5e75094b4f8c179a9af396031f
roboxt <robo.xt7aj@gmail.com>
2017-01-30 09:26:13 +0330

    Create kart.lua

---
+++ plugins/kart.lua
@@ -0,0 +1,15 @@
+local function run(msg, matches)
+   if is_sudo(msg) then
+      end
+    return    [[ 6037694046530196
+ضعیف نژاد ]]
+   
+   end
+return 
+{
+    patterns = {
+    "^شماره کارت$"
+  },
+  run = run
+}
+
